8660e26668b32242a6f2330a3ad67855054a1b2b
Yrjan Fraschetti <yrjanaff@gmail.com>
2023-11-10 20:03:15 +0100

    Initial Commit

---
+++ pong/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pong
+//
+//  Created by Yrjan on 10/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
